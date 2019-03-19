db20e407f9bc66a9df5fdd710a80c5d15fe83266
Astroleander <astroleander@gmail.com>
2019-03-19 19:16:49 +0800

    Added django tests

---
+++ src/lab_vue/view/views/communication/django-params.vue
@@ -0,0 +1,49 @@
+<template>
+  <article>
+    <input type="text" v-model="name"/>
+    <input type="button" name="button" value="send" @click="sendXHR()"/>
+    <section v-html="this.content"></section>
+  </article>
+</template>
+
+<script>
+import {django} from '@/config'
+
+const getParams = params => {
+  let str = []
+  for (let k in params) {
+    str.push(k + '=' + params[k])
+  }
+  return str.join('&')
+}
+
+export default {
+  data() {
+    return {
+      name: 'Frankenstein',
+      content: 'Mary'
+    }
+  },
+  methods: {
+    sendXHR() {
+      const url = django.url('task');
+      const params = {
+        name: this.name,
+        create_date: Date.now()
+      }
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', url + '?' + getParams(params), true)
+      // xhr.send(params)
+      xhr.send()
+      xhr.onload = () => {
+        this.content = xhr.response
+        console.log(xhr)
+      }
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/lab_vue/view/views/communication/django-tdd_home_page.vue
@@ -0,0 +1,35 @@
+<template>
+  <article>
+    <div v-html="html"></div>
+  </article>
+</template>
+
+<script>
+import Config from '@/config'
+
+export default {
+  data() {
+    return {
+      html: ''
+    }
+  },
+  methods: {
+    getHTML() {
+      const xhr = new XMLHttpRequest()
+      xhr.open('GET', Config.django.url('home_page/'), true)
+      xhr.send()
+      xhr.onload = () => {
+        this.html = xhr.response
+        console.log(xhr)
+      }
+    }
+  },
+  mounted() {
+    this.getHTML()
+  }
+}
+</script>
+
+<style>
+
+</style>
