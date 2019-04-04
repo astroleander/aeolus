f829797025d9acb756121c74028df9a4bdc71ff0
Astroleander <astroleander@gmail.com>
2019-04-05 01:17:53 +0800

    509

---
+++ src/lab_vue/view/views/algorithm/fibonacci.vue
@@ -0,0 +1,36 @@
+<template>
+  <article>
+    <input v-model.number="N"/>
+    <p>{{result}}</p>
+  </article>
+</template>
+
+<script>
+import Fib from '@lv/view/src/algo/leetcode-509-fibonacci'
+
+export default {
+  data() {
+    return {
+      N: 10,
+      result: null
+    }
+  },
+  methods: {
+    fib() {
+      this.result = Fib.fib_table(this.N)
+    }
+  },
+  mounted() {
+    this.fib()
+  },
+  watch: {
+    N() {
+      this.fib()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
