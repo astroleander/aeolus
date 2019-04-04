d4cd2559a684d67a11c837163c8a6cecabef79d3
Astroleander <astroleander@gmail.com>
2019-04-05 00:55:23 +0800

    Added arg and modifiers infos

--- src/lab_vue/view/views/vue/vue-directives.vue
+++ src/lab_vue/view/views/vue/vue-directives.vue
@@ -5,16 +5,20 @@
     </section>
     <section>
     <!-- <p>Directives Test Queue:</p> -->
-    <p v-test:id="test">Here will be replaced</p>
+    <p v-test="test">Here will be replaced</p>
     <input type="text" v-focus v-model="test"/>
-
     </section>
+    
     <section>
       <p><code>bind</code>{{bind}}</p>
       <p><code>inserted</code>{{inserted}}</p>
       <p><code>update</code>{{update}}</p>
       <p><code>componentUpdated</code>{{componentUpdated}}</p>
       <p><code>unbind</code>{{unbind}}</p>
+    </section>
+
+    <section>
+      <div v-output:foo.a.b="test"></div>
     </section>
   </article>
 </template>
@@ -28,7 +32,7 @@ export default {
       update: '被绑定元素所在的模板更新时使用。无论绑定值是否变化都会更新。通过比较更新前后的绑定值，可以忽略不必要的模板更新。',
       componentUpdated: '被绑定元素所在模板元素完成一次更新周期时调用。',
       unbind: '只调用一次，指令与元素解绑时调用。',
-      test: ''
+      test: 'test'
     }
   },
   directives: {
@@ -39,11 +43,28 @@ export default {
       }
     },
     test: {
-      bind: function(el, binding, vnode, oldVnode) { console.log('[bind]', el, binding, vnode, oldVnode); console.log('[Bind]'); },
+      bind: function(el, binding, vnode, oldVnode) { 
+        console.log('[bind]', el, binding, vnode, oldVnode); console.log('[Bind]'); },
       inserted: function(el, binding, vnode, oldVnode) { console.log('[inserted]')},
-      update: function(el, binding, vnode, oldVnode) { console.log('[update]'); el.innerHTML = binding.value },
+      update: function(el, binding, vnode, oldVnode) { 
+        console.log('[update]'); 
+        el.innerHTML = binding.value 
+        },
       componentUpdated: function(el, binding, vnode, oldVnode) { console.log('[componentUpdated]') },
       unbind: function(el, binding, vnode, oldVnode) { console.log('[unbind]') },
+    },
+    output: {
+      bind: function(el, binding, vnode, oldVnode) {
+        let s = JSON.stringify
+        el.innerHTML = 
+          'name: '        + s(binding.name)       + '<br>' +
+          'value: '       + s(binding.value)      + '<br>' +
+          'expression: '  + s(binding.expression) + '<br>' +
+          'argument: '    + s(binding.arg)        + '<br>' +
+          'modifiers: '   + s(binding.modifiers)   + '<br>' +
+          'vnode keys: '  + Object.keys(vnode).join('. ')
+
+      }
     }
   },
   methods:{
