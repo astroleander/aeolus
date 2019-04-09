39464878f3f71a15211577470e4e6dd263734d51
Astroleander <astroleander@gmail.com>
2019-04-09 23:40:17 +0800

    Added way of handle event

---
+++ src/lab_vue/view/views/vue/3_ways_to_handle_event.vue
@@ -0,0 +1,32 @@
+<template>
+  <article>
+    <input @click="handle" type="button" value="fuck 1">
+    <input @click="(e)=> handle(e,2)" type="button" value="fuck 2">
+    <input @click="handle($event,3)" type="button" value="fuck 3">
+    <p>
+      {{event}}
+      {{idx}}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: null,
+      idx: 0
+    }
+  },
+  methods: {
+    handle(e, idx) {
+      this.event = e
+      this.idx = idx 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
