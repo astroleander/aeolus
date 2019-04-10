8a0d30cec25d95e27ec939488b777f5db402db77
Astroleander <astroleander@gmail.com>
2019-04-10 17:08:26 +0800

    Everyone should try vue 2.6

---
+++ src/lab_vue/view/views/vue/newly_slot.vue
@@ -0,0 +1,27 @@
+<!-- Testing new v-slot added in vue 2.6.0 -->
+<template>
+  <article>
+    <aeo-card>
+      <template v-slot:header>
+        v-slot header
+      </template>
+      <template v-slot>
+        v-slot default
+      </template>
+    </aeo-card>
+  </article>
+</template>
+
+<script>
+import AeoCard from '@lv/view/components/common/Card'
+
+export default {
+  components: {
+    AeoCard
+  },
+}
+</script>
+
+<style>
+
+</style>
