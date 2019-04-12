e30e47c46adc2c1f994180dbe727bddfd95f3f11
Astroleander <astroleander@gmail.com>
2019-04-12 10:36:41 +0800

    Added transition effect

--- src/lab_vue/VueApp.vue
+++ src/lab_vue/VueApp.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view/>
+  <transition mode="out-in">
+    <router-view/>
+  </transition>
 </template>
 
 <script>
@@ -22,4 +24,14 @@ export default {
 html {
   background: repeating-linear-gradient(45deg, #FC5500 0, #FC5500 30px, transparent 30px, transparent 60px);
 }
+
+.v-enter, .v-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+}
+
+.v-enter-active, .v-leave-active {
+  transition: all .3s;
+}
+
 </style>
