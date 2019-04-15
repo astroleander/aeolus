9acd3a3e1172789e6280da62df2c3cb664ae3382
Astroleander <astroleander@gmail.com>
2019-04-16 00:34:15 +0800

    Created base progress bar

---
+++ src/lab_vue/view/views/view/component_progress.vue
@@ -0,0 +1,32 @@
+<template>
+  <article class="flex-middle">
+    <aeo-progress-bar></aeo-progress-bar>
+    <aeo-progress-bar v-model="test"></aeo-progress-bar>
+    {{test.value}} / {{test.total}}
+    <button @click="()=>{this.test.value+=40}">++</button>
+  </article>
+</template>
+
+<script>
+import AeoProgressBar from '@lv/view/components/common/ProgressBar'
+
+export default {
+  data() {
+    return {
+      test: {
+        total: 200,
+        value: 100,
+      }
+    }
+  },
+  components: {
+    AeoProgressBar
+  }
+}
+</script>
+
+<style>
+article {
+  width: 80%;
+}
+</style>
