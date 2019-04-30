135cf717e41fa8bd914ab72fbfde7c931220ff64
Astroleander <astroleander@gmail.com>
2019-04-30 11:39:17 +0800

    Fixed name

---
+++ src/lab_vue/view/views/vue/v-model_customize.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="flex-middle">
+    <h4>using v-model on custom component</h4>
+    <custom-input v-model="searchText"></custom-input>
+    {{searchText}}
+  </article>
+</template>
+
+<script>
+import Vue from 'vue'
+import CustomInput from '@lv/view/components/sample/CustomInput'
+
+export default {
+  components: {
+    'custom-input': CustomInput
+  },
+  data() {
+    return {
+      searchText: null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
