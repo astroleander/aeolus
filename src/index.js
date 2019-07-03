a0a57dc884ba9957fa53893a493e530265d845bf
Astroleander <astroleander@gmail.com>
2019-07-03 14:50:46 +0800

    draw flask

--- src/index.js
+++ src/index.js
@@ -10,10 +10,29 @@
  * 3. 默认 production 和 development 模式
  */
 import style from '#res/main.css';
-
+import '#res/styles/indexpage.scss'
 console.log(`I'm a silly entry`);
 console.log(`HtmlWebpackPlugin 里配置 chunks 来实现按需加载 js`);
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Dev Mode!')
 }
+
+animateBubble()
+animateWater()
+
+function animateBubble() {
+  let e = document.getElementsByClassName('bubble-in-flask')
+  for (let index = 0; index < e.length; index++) {
+    let el = e[index]
+    let speed = 1 / el.r.baseVal.value
+    setInterval(()=>{
+      el.cy.baseVal.value < 0 ? 
+        el.cy.baseVal.value = 100 : el.cy.baseVal.value -= speed
+    }, 10)
+  
+  }
+ }
+ function animateWater() {
+   let p = document.getElementsByClassName('waterline-in-flask')
+ }
