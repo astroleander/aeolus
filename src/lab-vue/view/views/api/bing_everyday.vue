97cf3768467aeb90e951dbef9f19d737425c6d3a
Astroleander <astroleander@gmail.com>
2019-07-09 15:19:51 +0800

    fixed white gap at bottom of img tag

--- src/lab-vue/view/views/api/bing_everyday.vue
+++ src/lab-vue/view/views/api/bing_everyday.vue
@@ -1,6 +1,6 @@
 <template>
-  <article>
-    <img :src="this.res" width="100%"/>
+  <article class="fullscreen">
+    <img :src="this.res" width="100%" height="100%"/>
   </article>  
 </template>
 
@@ -26,5 +26,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+img {
+  object-fit: cover;
+}
 </style>
