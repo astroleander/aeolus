a3b1bccd6dc52a756bcbe60a3ddff1f57a864495
Astroleander <astroleander@gmail.com>
2019-07-09 15:11:54 +0800

    split algo from vue page

--- src/lab-vue/view/views/algorithm/pascals_triangle_II.vue
+++ src/lab-vue/view/views/algorithm/pascals_triangle_II.vue
@@ -16,41 +16,7 @@
 </template>
 
 <script>
-function table(numRows) {
-  if (numRows < 1) return [];
-  const array = [[1]];
-  for (let row = 0; row < numRows; row++) {
-    array.push([]);
-    for (let idx = 0; idx < row + 1; idx++) {
-      if (row === 0) continue;
-      array[row].push( 
-        (array[row - 1] && array[row - 1][idx - 1] || 0) +
-        (array[row - 1] && array[row - 1][idx] || 0)
-      )
-    }
-  }
-  array.pop()
-  return [array[array.length - 1], [].concat(...array).length]
-}
-
-/**
- * 其实上面那个也是 dp, 这里只是按照题目[1660]要求优化至 O(n) 的空间复杂度
- * https://leetcode.com/explore/featured/card/recursion-i/251/scenario-i-recurrence-relation/1660/
- */
-function dp(numRows) {
-  if (numRows < 1) return [];
-  let array = [1];
-  let newarr
-  for(let i = 0; i < numRows + 1; i++) {
-    // 第 i 行有 i 个数
-    newarr = [1]
-    for (let j = 1; j < i; j++) {
-      newarr[j] = (array[j-1] || 0) + (array[j] || 0)
-    }
-    array = newarr
-  }
-  return [array, newarr.length + array.length]
-}
+import { dp, table } from '#algo/leetcode-118-119-pascals-triangle'
 
 export default {
   data() {
@@ -60,9 +26,11 @@ export default {
   },
   computed: {
     printDp() {
+      console.log(dp, table)
       return dp(this.times)
     },
     printTable() {
+      console.log(this.times)
       return table(this.times)
     }
   }
