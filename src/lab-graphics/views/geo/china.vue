64e364bcb8e17e4ffc02e3bab5ba206f0e1c27dd
Astroleander <34837604+Astroleander@users.noreply.github.com>
2019-07-23 00:18:24 +0800

    draw svg china map

--- src/lab-graphics/views/geo/china.vue
+++ src/lab-graphics/views/geo/china.vue
@@ -1,24 +1,49 @@
 <template>
-  <article class="fullscreen" v-html="data">
+  <article class="fullscreen">
+    <section class="fullscreen" id="chinamap" v-html="map">
+
+    </section>
   </article>
 </template>
 
 <script>
-import chinaMap from '#res/assets/china.svg'
+import ChinaMap from '@/res/assets/china.svg'
 export default {
+  name: 'svg-sample',
   data() { 
     return {
-      data: chinaMap
-    } 
+      map: ChinaMap
+    }
+  },
+  mounted() {
+    let map = document.getElementById('svg-china-map');
+    console.log(map);
+    let node = map.getElementById('CN-50')
+    console.log(node)
+    let nodebox = node.getBBox()
+    console.log(nodebox)
+    let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.setAttribute('x', nodebox.x + nodebox.width / 2);
+    text.setAttribute('y', nodebox.y + nodebox.height / 2);
+    text.setAttribute('fill', '#000');
+    text.textContent = node.getAttribute('title');
+    map.appendChild(text)
   }
 }
 </script>
 
-<style scoped lang='scss'>
-svg path {
-  fill: lightcoral；
-}
-svg {
-  width: 1000px;
+<style lang='scss'>
+#chinamap  {
+  svg {
+    width: 774px;
+    min-height: 570px;
+  }
+  path {
+    transition: all 0.2s;
+    fill: lightcoral;
+    &:hover {
+      fill: lightblue;
+    }
+  }
 }
 </style>
