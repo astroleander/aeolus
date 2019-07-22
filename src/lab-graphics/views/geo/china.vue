f46d4f86be7cd31341bcaced7cb79fdaf609fc2c
Astroleander <astroleander@gmail.com>
2019-07-22 17:30:04 +0800

    staged for remote

---
+++ src/lab-graphics/views/geo/china.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="fullscreen" v-html="data">
+  </article>
+</template>
+
+<script>
+import chinaMap from '#res/assets/china.svg'
+export default {
+  data() { 
+    return {
+      data: chinaMap
+    } 
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+svg path {
+  fill: lightcoral；
+}
+svg {
+  width: 1000px;
+}
+</style>
