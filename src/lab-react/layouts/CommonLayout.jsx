fbfaf7f05dfffa0d6c40801d9e65aeaff5823f3d
Astroleander <astroleander@gmail.com>
2019-09-20 17:28:30 +0800

    unit nav float button style

--- src/lab-react/layouts/CommonLayout.jsx
+++ src/lab-react/layouts/CommonLayout.jsx
@@ -3,9 +3,30 @@ import React, { Component } from 'react'
 export default class CommonLayout extends Component {
   render() {
     return (
-      <div>
-        C O M M O N
+      <article>
+        
+        <FloatButton />
+      </article>
+    )
+  }
+}
+
+class FloatButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rotateDeg: 45
+    }
+  }
+  render() {
+    return (
+      <div className='floating-button' id='react-common-layout-floating-button'>
+        <span className='cross'>+</span>
       </div>
     )
   }
+}
+
+const floating_button = {
+  
 }
