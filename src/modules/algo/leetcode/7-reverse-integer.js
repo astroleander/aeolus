88bafe01fcdd37758f0259ff83b12147dc1f819f
Astroleander <astroleander@gmail.com>
2019-09-25 17:06:05 +0800

    add first solutions

---
+++ src/modules/algo/leetcode/7-reverse-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let result = 0; 
+  let ng = false;
+  if (x < 0) { 
+      x = Math.abs(x); 
+      ng = true; 
+    }
+  while (x !== 0) {
+    result = result * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+  return ng ? 
+    -result < -2147483648? 0: -result :
+    result > 2147483647? 0: result
+};
+console.log(reverse(-321));
