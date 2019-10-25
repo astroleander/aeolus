0a525b68592c7103e3297a8bdecbb6f069ebb234
Astroleander <astroleander@gmail.com>
2019-10-26 01:09:31 +0800

    reverse shift direction

--- src/modules/algo/leetcode/190-reverse-bits.js
+++ src/modules/algo/leetcode/190-reverse-bits.js
@@ -7,16 +7,16 @@ var reverseBits = function(n) {
     let result = 0;
     let idx = 31;
     while (x < 4294967296) {
-        console.log((n & x ? 1 : 0) * (2 >> idx))
-        result += (n & x ? 1 : 0) * (2 >> idx);
+        console.log((n & x ? 1 : 0) * (2 << idx))
+        result += (n & x ? 1 : 0) * (2 << idx);
         // x <<= 1;
         x *= 2;
         idx--;
     }
     return result;
 };
-console.log(reverseBits(2048))
+console.log(reverseBits(4))
 module.exports = {
     reverseBits,
-    inputs: [2048]
+    inputs: [4]
 }
