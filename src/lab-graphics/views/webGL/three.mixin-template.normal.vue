fa891272734e6cedd0ac4358ad943305012aac97
Astroleander <astroleander@gmail.com>
2019-10-30 12:51:00 +0800

    add mixin template  for three.js

---
+++ src/lab-graphics/views/webGL/three.mixin-template.normal.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id='container'>
+    
+  </section>
+</template>
+
+<script>
+import mixin from '@/modules/utils/mixin.template.three.normal.js'
+export default {
+  mixins: [mixin],
+  mounted() {
+    this.setRenderTarget(document.getElementById('container'))
+  }
+}
+</script>
+
+<style>
+
+</style>
