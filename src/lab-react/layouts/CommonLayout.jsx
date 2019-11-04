c40fb29b2c297e6f6868424e513a863702415d3d
Astroleander <astroleander@gmail.com>
2019-11-04 21:00:51 +0800

    supported loading components dynamically

--- src/lab-react/layouts/CommonLayout.jsx
+++ src/lab-react/layouts/CommonLayout.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 
 export default class CommonLayout extends Component {
   constructor(props) {
     super(props);
+    this.state = {}
   }
+  static getDerivedStateFromProps(props, state) {
+    let pathname = props.location.pathname
+    let path = pathname.substring(pathname.indexOf('/', 2));
+    state = {
+      path
+    }
+    return state
+  }
+  
   render() {
+    {
+      // import('@r/views' + this.state.path).then(m => {
+      //   console.log(m)
+      // })
+    }
+
+    const DynamicComponent = React.lazy(() => import('@r/views' + this.state.path))
+    function MyComponent() {
+      return (
+        <Suspense fallback={<div>loading...</div>}>
+          <DynamicComponent />
+        </Suspense>
+      );
+    }    
     return (
       <article>
-        
+        <MyComponent />
         <FloatButton />
       </article>
     )
