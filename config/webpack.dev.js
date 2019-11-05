a33460dffe85415c32aaa5f61bc6aa7671541399
Astroleander <astroleander@gmail.com>
2019-11-05 19:56:28 +0800

    commented process

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  /**
+   * 这个值会被打在 process.env.NODE_ENV 上, 顾名思义, 这个值实际不是 webpack 提供的, 而是 Node 提供的,
+   * 在打包时这个值被编译成变量
+   */
   mode: 'development',
 //   devtool: 'inline-source-map',
   devServer: {
